1dbbe3272dfe1aa837cd2a367eec97a41a693d38
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-01-15 14:04:27 +0100

    ADD function index to API/ProjectController

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function index(Request $request) 
+    {
+        $projects = Project::all();
+        return response()->json([
+            'success' => true,
+            'results' => $projects
+        ]);
+    }
 }
