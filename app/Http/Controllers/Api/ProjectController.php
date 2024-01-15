e0b679c74994b16c0ee2c1840f642acf00a54460
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-01-15 19:42:14 +0100

    Refactor ProjectController

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -11,11 +11,13 @@ class ProjectController extends Controller
     public function index(Request $request) 
     {
         $data = $request->all();
-        dd($data);
-        $results = Project::with('type', 'tecnologies', 'user')->paginate($data['page']);
+
+        $results = Project::with('type', 'tecnologies', 'user')->paginate($data['qtyForPage']);
+
         return response()->json([
             'results' => $results,
-            'success' => true
+            'success' => true,
+            'request' => $data
         ]);
     }
 }
