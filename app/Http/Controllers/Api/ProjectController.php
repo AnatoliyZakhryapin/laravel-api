b4cc74a069980ec634ae359ddb69928064a83fd3
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-01-17 22:06:47 +0100

    ADD possibility to select project with types null

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Project;
 use App\Models\Tecnology;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
@@ -13,20 +14,32 @@ class ProjectController extends Controller
     {
         $data = $request->all();
 
-
         $results_projects = Project::with('type', 'tecnologies', 'user');
         
-        // if (true) {
-        //     $results_projects = $results_projects->where('type_id', '2');
+        if (isset($data['formRequest']['typesSelected'])) {
+            $results_projects = $results_projects->whereIn('type_id', $data['formRequest']['typesSelected']);
+        }
+
+        if (isset($data['formRequest']['typesSelectedNull'])) {
+            $results_projects = $results_projects->orWhereNull('type_id', '');
+        }
+
+
+        // if (isset($data['formRequest']['tecnologiesSelected'])) {
+        //     $results_projects = $results_projects->whereIn('type_id', $data['formRequest']['tecnologiesSelected']);
         // }
+
+        // dd($results_projects->get());
 
         $results_projects = $results_projects->paginate($data['qtyForPage']);
         $tecologies = Tecnology::all();
+        $types = Type::all();
 
         return response()->json([
             'results' => [
                 'projects' => $results_projects,
                 'tecnologies' => $tecologies,
+                'types' => $types
             ],
             'success' => true,
             'request' => $data,
